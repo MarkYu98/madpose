15f70fca0b83c196c659b0a1dbfc394d4085a3f4
B1ueber2y <b1ueber2y@gmail.com>
2025-01-07 11:02:04 +0100

    specify clang-format version

--- scripts/clang_format.sh
+++ scripts/clang_format.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
 
+# Find clang-format
+tools='
+  clang-format-8
+  clang-format
+'
+
+clang_format=''
+for tool in ${tools}; do
+    if type -p "${tool}" > /dev/null; then
+        clang_format=$tool
+        break
+    fi
+done
+
+if [ -z "$clang_format" ]; then
+    echo "Could not locate clang-format"
+    exit 1
+fi
+echo "Found clang-format: $(which  ${clang_format})"
+
+# Check version
+version_string=$($clang_format --version | sed -E 's/^.*(\d+\.\d+\.\d+-.*).*$/\1/')
+expected_version_string='19.1.0'
+if [[ "$version_string" =~ "$expected_version_string" ]]; then
+    echo "clang-format version '$version_string' matches '$expected_version_string'"
+else
+    echo "clang-format version '$version_string' doesn't match '$expected_version_string'"
+    exit 1
+fi
+
 # Get all C++ files checked into the repo under src/
 root_folder=$(git rev-parse --show-toplevel)
 all_files=$( \
